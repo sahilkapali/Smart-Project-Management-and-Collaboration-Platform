c0f0793c81f106deec8bbbb45522b229e1ff07df
Naomi Rai <naomirai.080@kathford.edu.np>
2026-08-17 12:57:56 +0545

    fix frontend API proxy

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken, removeToken } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "/api",
   withCredentials: true,
 });
 
